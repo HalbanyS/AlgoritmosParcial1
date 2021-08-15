e1e592f6f4e24f9f7eac7edeab4667a5002f6b84
HalbanyS <62803617+HalbanyS@users.noreply.github.com>
2021-08-15 13:07:30 -0600

    Add files via upload

---
+++ Primer Parcial Algoritmos/temperatura.py
@@ -0,0 +1,25 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+#programa realizado por Halbany Sanchez
+#ingnieria en sistemas
+#item 13
+#grados Farenhit a Celcius
+print("BIENVENIDO")
+print("PUEDES CONVERTIR GRADOS fahrenheit A celsius")
+def fahrenheit_celsius(f):
+    
+    return (f - 32) * 5.0/9.0
+n1 = float(input("ingrese los grados fahrenheit_"))
+print("--------------------")
+print("RESULTADO EN GRADOS celsius")
+print(fahrenheit_celsius(n1)) 
+
+print("--------------------")
+print("-------------Realizado por Halbany Sanchez-------")
+
